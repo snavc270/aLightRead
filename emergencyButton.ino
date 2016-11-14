ac59ee7f680f4648a4b7b0e1a2424cd7bb047d3c
snavc270 <snavc270@newschool.edu>
2016-11-14 18:58:10 -0500

    thirdCommit

--- emergencyButton.ino
+++ emergencyButton.ino
@@ -14,15 +14,12 @@ int randomNum;
 boolean firstPressed = false;
 int charInt = 0;
 int percentage = 100; 
-int storyInt = 2; 
+int storyInt = 1; 
 
 //String story[5] = {"hello", "how", "are", "you", "?"};
 String story[28] = {"You", "want", "to", "be", "a", "master ", "he", "said", "well",  "you","are", "not", "a", "master ", "That", "took", "me", "down", "a", "peg.", "Seems", "I", "still", "have", "a", "lot", "to", "learn"};
-String story2[138] = {"She", "likes", "it", "She", "is", "like", "me", "Therefore", "I", "might", "like", "it", "She", "is", "like", "me", "She", "likes", "the", "things", "I", "like", "She", "likes", "this", "So", "I", "might", "like", "it",
-                    "I", "like", "it", "I", "show", "it", "to", "her", "She", "likes", "it", "She", "is", "like", "me", "Therefore", "I", "might", "really", "like", "it", "I", "think", "I", "like", "it", "I", "show", "it", "to", "her",
-                    "She", "likes", "it", "She", "is", "like", "me", "Therefore", "I", "might", "really", "like", "it", "I", "think", "I", "like", "it", "I", "show", "it", "to", "her", "She", "is", "like", "me", "She", "likes", "the", "things", 
-                    "I", "like", "She", "likes", "it", "So", "I", "might", "really", "like", "it", "I", "like", "it", "I", "show", "it", "to", "her", "She", "likes", "it", "She", "says", "the", "other", "one", "is", "just", "plain", "awful", "She"
-                    "is", "like", "me", "She", "likes", "the", "things", "I", "like", "So", "I", "might", "really", "like", "it"}; 
+//String story2[30] = {"She", "likes", "it", "She", "is", "like", "me", "Therefore", "I", "might", "like", "it", "She", "is", "like", "me", "She", "likes", "the", "things", "I", "like", "She", "likes", "this", "So", "I", "might", "like", "it"}; 
+                    
 String story3[28] = {"Into", "how", "small", "a", "place", "the", "word", "judgement", "can", "be", "compressed", "it", "must", "fit", "inside", "the", "brain", "of", "a", "ladybug", "as", "she", 
                   "before", "my", "eyes", "makes", "a", "decision"}; 
 
@@ -35,7 +32,7 @@ String story5[10] = {"Under", "all", "this", "dirt", "the", "floor", "is", "real
 void setup() {
   pinMode(7, OUTPUT); digitalWrite(7, LOW);
   // put your setup code here, to run once:
-//  Serial.print(story4.length); 
+  Serial.begin(19200); 
   mySerial.begin(19200);
   printer.begin();
 
@@ -62,50 +59,83 @@ void setup() {
 
 }
 
-void dump() {
-//  uint8_t major, minor, c;
-//
-//  printer.println(F("        01234567  89ABCDEF"));
-//  for (major = 0; major < 16; major++) {
-//    printer.print(F("     "));
-//    printer.print(major, HEX);
-//    printer.print(F("- "));
-//    for (minor = 0; minor < 16; minor++) {
-//      c = (major << 4) | minor;
-//      if (c < 32) c = ' '; // Skip control codes!
-//      printer.write(c);
-//      if (minor == 7) printer.print(F("  "));
-//    }
-//    printer.println();
-//  }
-}
-
 void loop() {
 
   buttonState = digitalRead(buttonPin);
-  //  Serial.println(firstPressed);
 
-  //  if(buttonState != previousButtonState){
-  //    firstPressed = true;
-  //  }else{
-  //    firstPressed = false;
-  //  }
-  //
-  //  if(firstPressed){
   switch(storyInt){
     case 1:
+      percentage = map(charInt, 0, 27, 90, 2); 
+      Serial.println(percentage); 
       {
+        if (buttonState != previousButtonState) {
+          if (buttonState == 1) {
+            int randomNum = random(0, 100);
+            
+            if (randomNum < percentage) {
+              printer.println(story[charInt]);          
+    //        mySerial.println(story[charInt]);
+              if(percentage> 50){
+                percentage -=10; 
+              }else if (percentage <= 50 && percentage > 10){
+                percentage -=7; 
+              }else if (percentage<= 10 && percentage >2){
+                percentage -= 2; 
+              }
+              if (charInt < 27) {
+                charInt ++;
+              }
+            } else {
+              mySerial.println(".");
+            }
+         }
+        }
+        if(charInt == 27){
+            storyInt = 2; 
+       }
+      break; 
+      }
+//      case 2: {
+//         if (buttonState != previousButtonState) {
+//          if (buttonState == 1) {
+//            int randomNum = random(0, 100);
+//           
+//            if (randomNum < percentage) {
+//              printer.print(story2[charInt]); 
+//      //        mySerial.println(story[charInt]);
+//              if(percentage> 70){
+//                percentage -=5; 
+//              }else if (percentage <= 70 && percentage > 10){
+//                percentage -=3; 
+//              }else if (percentage<= 10 && percentage >2){
+//                percentage -= 2; 
+//              }
+//              if (charInt < 28) {
+//                charInt ++;
+//              }
+//            } else {
+//              mySerial.println(".");
+//            }
+//         }
+//      }
+//      if(charInt == 29){
+//            storyInt = 3; 
+//         }
+//      break; 
+//      }
+
+      case 3: {
         if (buttonState != previousButtonState) {
           if (buttonState == 1) {
             int randomNum = random(0, 100);
            
             if (randomNum < percentage) {
-              printer.println(story[charInt]); 
+              printer.print(story3[charInt]); 
       //        mySerial.println(story[charInt]);
-              if(percentage> 50){
-                percentage -=10; 
-              }else if (percentage <= 50 && percentage > 10){
-                percentage -=7; 
+              if(percentage> 70){
+                percentage -=5; 
+              }else if (percentage <= 70 && percentage > 10){
+                percentage -=3; 
               }else if (percentage<= 10 && percentage >2){
                 percentage -= 2; 
               }
@@ -117,15 +147,18 @@ void loop() {
             }
          }
       }
+      if(charInt == 27){
+            storyInt = 4; 
+      }
       break; 
       }
-      case 2: {
-         if (buttonState != previousButtonState) {
+      case 4: {
+        if (buttonState != previousButtonState) {
           if (buttonState == 1) {
             int randomNum = random(0, 100);
            
             if (randomNum < percentage) {
-              printer.print(story2[charInt]); 
+              printer.print(story4[charInt]); 
       //        mySerial.println(story[charInt]);
               if(percentage> 70){
                 percentage -=5; 
@@ -134,13 +167,45 @@ void loop() {
               }else if (percentage<= 10 && percentage >2){
                 percentage -= 2; 
               }
-              if (charInt < 137) {
+              if (charInt < 44) {
                 charInt ++;
               }
             } else {
               mySerial.println(".");
             }
          }
+      }
+      if(charInt == 45){
+            storyInt = 5; 
+      }
+      break; 
+      }
+
+      case 5: {
+        if (buttonState != previousButtonState) {
+          if (buttonState == 1) {
+            int randomNum = random(0, 100);
+           
+            if (randomNum < percentage) {
+              printer.print(story5[charInt]); 
+      //        mySerial.println(story[charInt]);
+              if(percentage> 70){
+                percentage -=5; 
+              }else if (percentage <= 70 && percentage > 10){
+                percentage -=3; 
+              }else if (percentage<= 10 && percentage >2){
+                percentage -= 2; 
+              }
+              if (charInt < 8) {
+                charInt ++;
+              }
+            } else {
+              mySerial.println(".");
+            }
+         }
+      }
+      if(charInt == 9){
+            storyInt = 1; 
       }
       break; 
       }
@@ -150,6 +215,6 @@ void loop() {
   }
   
   previousButtonState = buttonState;
-//  delay(50); 
+
 
 }
